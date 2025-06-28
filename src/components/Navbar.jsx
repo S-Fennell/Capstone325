5ad5b5735308e154b913a983d5fcd036bd84d31f
S-Fennell <67168523+S-Fennell@users.noreply.github.com>
2025-06-28 15:55:54 -0400

    creating a nav bar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+function Navbar(){
+
+
+    return(
+        <nav className="nav">
+       <h1>Fit Life</h1>
+       <div className="links">
+        <a href="">Home</a>
+        <a href="">Home</a>
+        <a href="">Subcribe</a>
+        <a href="">Log In</a>
+       </div>
+        </nav>
+    )
+
+}
+export default Navbar
