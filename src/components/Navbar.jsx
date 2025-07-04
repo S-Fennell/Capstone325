ff1d4809ca496399a0b567bda8bde981d297f099
S-Fennell <67168523+S-Fennell@users.noreply.github.com>
2025-07-04 10:01:09 -0400

    updated navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,37 @@
-function Navbar(){
+import { useState } from 'react'
 
+function Navbar(){
+  const [selectedValue, setSelectedValue] = useState(''); // State to hold the selected value
+
+  const handleChange = (event) => {
+    setSelectedValue(event.target.value); // Update state on change
+  };
 
     return(
-        <nav className="nav">
-       <h1>Fit Life</h1>
-            <div className="links">
-                    <a href="">Home</a>
-                    <a href="">Home</a>
-                    <a href="">Subcribe</a>
-                    <a href="">Log In</a>
-            </div>
-        </nav>
-    )
-
+      <nav className="navbar">
+        <div class="fit">
+            <h1>Fit 🏃🏽‍♂️‍➡️🏃🏼‍♀️‍➡️ Life</h1>
+        </div>
+        
+        <div className="links">
+            <a className='anc' href="/">Home</a>
+            <a className='anc' href="/diet">Diet & Nutrition</a>
+            <a className='anc' href="/fatlose">Fat Loss</a>
+            
+             <label htmlFor="dropDown"></label>
+            <select className="dropDown" value={selectedValue} onChange={handleChange}>
+                <option value="">Types of Training</option>
+                <option value="Build Endurance"><a href="">Build Endurance</a></option>
+                <option value="Build Muscle"><a href="">Build Muscle</a></option>
+                <option value="Build Strength"><a href="">Build Strength</a></option>
+            </select>
+            <a className='anc, sign' href="">Sign Up |</a>
+            <a href=""> Sign In</a>
+            {selectedValue && <p>Let's {selectedValue}</p>}
+            
+        </div>
+      </nav>
+    );
 }
+
 export default Navbar
