a4e0b998e18795ad866d3e31c19dd9a719de057f
S-Fennell <67168523+S-Fennell@users.noreply.github.com>
2025-07-22 14:26:57 -0400

    got rid of drop down

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,13 +19,7 @@ function Navbar(){
             <Link to="/diet"><div className='anc'>Diet & Nutrition</div></Link>
             <Link to="/fatloss"><div className='anc'>Fat Loss</div></Link>
             <Link to="/strength"><div className='anc'>Build Strength</div></Link>
-            
-            <select className="dropDown" value={selectedValue} onChange={handleChange}>
-                <option value="">Types of Training</option>
-                <option value="Build Endurance"><Link to="">Build Endurance</Link></option>
-                <option value="Build Muscle"><Link to="">Build Muscle</Link></option>
-                <option value="Build Strength"><Link to="">Build Strength</Link></option>
-            </select>
+           
             <Link to="/subscribe"><div className='anc, sign'>Subscribe</div></Link>
           
             {selectedValue && <p>Let's {selectedValue}</p>}
