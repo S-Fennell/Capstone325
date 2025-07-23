a6ce11769ae5ee20d406f64e74c08aaaac2207f9
S-Fennell <67168523+S-Fennell@users.noreply.github.com>
2025-07-23 17:29:39 -0400

    added more images for lifestyle page

--- src/pages/LifeStyleHabits.jsx
+++ src/pages/LifeStyleHabits.jsx
@@ -8,7 +8,56 @@ export default function LifeStyleHabits(){
             </div>
               <div className="midbar">
                 <h1>Good Lifestyle Habits</h1>
-            </div>
+              </div>
+              <div>
+                <h3 className='topicBar'>
+                    Sleep | Exercise | Body weight | Healthy diet | Staying hydrated | No smoking | Nasal breathing
+                </h3>
+              </div>
+              <div className="contentContainer">
+
+                <div className="topic">
+                    <ing className="topicImg" src=""></ing>
+                    <h2>Sleep</h2>
+                    <p>Sleep</p>
+                </div>
+                               
+                <div className="topic">
+                    <ing className="topicImg" src=""></ing>
+                    <h2>Exercise</h2>
+                    <p>Exercise</p>
+                </div>
+                                <div className="topic">
+                    <ing className="topicImg" src=""></ing>
+                    <h2>Body weight</h2>
+                    <p>Body weight</p>
+                </div>
+
+                <div className="topic">
+                    <ing className="topicImg" src=""></ing>
+                    <h2>Healthy diet</h2>
+                    <p>Healthy diet</p>
+                </div>
+                               
+                <div className="topic">
+                    <ing className="topicImg" src=""></ing>
+                    <h2>Staying hydrated</h2>
+                    <p>Staying hydrated</p>
+                </div>
+                                
+                <div className="topic">
+                    <ing className="topicImg" src=""></ing>
+                    <h2>No smoking</h2>
+                    <p>No smoking</p>
+                </div>
+
+                <div className="topic">
+                    <ing className="topicImg" src=""></ing>
+                    <h2>Nasal breathing</h2>
+                    <p>Nasal breathing</p>
+                </div>
+                
+              </div>
         
         </>
     )
