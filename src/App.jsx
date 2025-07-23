69bcfa9925b82059ed565c67716fb3f00b5a3f03
S-Fennell <67168523+S-Fennell@users.noreply.github.com>
2025-07-23 10:34:05 -0400

    cleaning up files. however, somehow my sign in file is ancored to my subscription file so I'll leave it till I figure out how why this effect is happening

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import HeroImage from './components/HeroImage';
 import GridImageLink from './components/GridImageLinks';
 import InfoBar from './components/InfoBar';
 import './App.css';
-import SignIn from './pages/signIn';
+// import SignIn from './pages/signIn';
 import { Link } from 'react-router-dom';
 import { Route, Routes } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
--- src/pages/signIn.jsx
+++ src/pages/signIn.jsx
@@ -1,5 +1,5 @@
 
-
+//Somehow this page is ancored to my sibscription page. If I delelte it, my sibscribption page will not load. Odd behavior.
 export default function SignIn(){
 //I only left this page because react seems to act up about deleting files. I'm just going with a subscribe page
     return(
